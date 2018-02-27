4de33396a291bad0acd8ee98f33b10ce9701b710
caro46 <cauretcaroline@gmail.com>
2018-02-27 09:46:08 -0500

    adding script for running links on iqaluk

---
+++ some_scripts/running_links_mello_canu.sh
@@ -0,0 +1,32 @@
+
+
+
+
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/pseudomolecules/allpaths/final.assembly.fasta -s mellotrop_pacbio.fof -b mellotrop_allpaths_links1 -d 5000 -t 200 -k 26 -l 5 -a 0.3 
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links1.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links2 -d 7500 -t 200 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links2.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links3 -d 10000 -t 150 -k 26 -l 5 -a 0.3 
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links3.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links4 -d 12500 -t 150 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links4.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links5 -d 15000 -t 150 -k 26 -l 5 -a 0.3 
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links5.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links6 -d 20000 -t 100 -k 26 -l 5 -a 0.3 
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links6.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links7 -d 30000 -t 75 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links7.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links8 -d 40000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links8.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links9 -d 50000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links9.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links10 -d 60000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links10.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links11 -d 70000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links11.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links12 -d 80000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links12.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links13 -d 90000 -t 50 -k 26 -l 5 -a 0.3
+
+/work/cauretc/programs/links_v1.8.5/LINKS -f /work/cauretc/2017_Mellotropicalis/scaffolding_links/mellotrop_allpaths_links13.scaffolds.fa -s mellotrop_pacbio.fof -b mellotrop_allpaths_links14 -d 100000 -t 50 -k 26 -l 5 -a 0.3
